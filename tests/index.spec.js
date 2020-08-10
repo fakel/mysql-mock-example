768e82570e4505ff1fea9271341cd4b391960105
Fares A. Akel C <f.antonio.akel@gmail.com>
2020-08-10 01:14:19 -0500

    Mock initial db connection

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,11 +1,28 @@
-const mysql = require('mysql');
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/no-hooks */
 const sinon = require('sinon');
-const mock = mock = sinon.mock(require('mysql'));
+const mock = sinon.mock(require('mysql'));
+
+// Lets Mock the initial connection
+mock.expects('createConnection').returns({
+  connect: () => {
+    console.log('Succesfully connected');
+  },
+  query: (query, vars, callback) => {
+    callback(null, query);
+  },
+  end: () => {
+    console.log('Connection ended');
+  },
+});
+
+const db = require('../index');
 
 describe('mySQL', () => {
-  it.todo('should invoke connect');
   it.todo('should create a db');
   it.todo('should create a table');
   it.todo('should insert into a table');
   it.todo('should select from a table');
+  it.todo('should close the connection');
 });
