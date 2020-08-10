cf23e27db345a275b308efe3cf98b6664aa2bcd1
Fares A. Akel C <f.antonio.akel@gmail.com>
2020-08-10 00:37:07 -0500

    Add Mysql and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,16 @@ module.exports = {
     commonjs: true,
     es2020: true,
     node: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/all',
   ],
   parserOptions: {
     ecmaVersion: 11,
   },
   rules: {
   },
+  plugins: ['jest'],
 };
