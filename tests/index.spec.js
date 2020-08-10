008d0c7f2301a07bce966d1ce201a57ad80ebbe0
Fares A. Akel C <f.antonio.akel@gmail.com>
2020-08-10 00:37:43 -0500

    Add tests boilerplate

---
+++ tests/index.spec.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const sinon = require('sinon');
+const { describe } = require('yargs');
+const { italic } = require('chalk');
+
+const mock = mock = sinon.mock(require('mysql'));
+
+describe('mySQL', () => {
+  it.todo('should invoke connect');
+  it.todo('should create a db');
+  it.todo('should create a table');
+  it.todo('should insert into a table');
+  it.todo('should select from a table');
+});
