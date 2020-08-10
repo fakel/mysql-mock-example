bb104cc64d616e970bc4a4010d397d7affaadeea
Fares A. Akel C <f.antonio.akel@gmail.com>
2020-08-10 03:02:16 -0500

    Replace query with a stub

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,17 +1,18 @@
+/* eslint-disable jest/expect-expect */
 /* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable jest/valid-expect */
 /* eslint-disable jest/no-hooks */
 const sinon = require('sinon');
-const mock = sinon.mock(require('mysql'));
+const mockMysql = sinon.mock(require('mysql'));
 
-// Lets Mock the initial connection
-mock.expects('createConnection').returns({
-  connect: () => {
-    console.log('Succesfully connected');
+const myStub = sinon.stub();
+
+mockMysql.expects('createConnection').returns({
+  connect: (cb) => {
+    console.log('Connection Mocked');
+    cb();
   },
-  query: (query, vars, callback) => {
-    callback(null, query);
-  },
+  query: myStub,
   end: () => {
     console.log('Connection ended');
   },
@@ -20,8 +21,14 @@ mock.expects('createConnection').returns({
 const db = require('../index');
 
 describe('mySQL', () => {
-  it.todo('should create a db');
-  it.todo('should create a table');
+  it('should create a table', (done) => {
+    myStub.callsFake((query, cb) => {
+      console.log(query);
+      cb();
+      done();
+    });
+    db.createTable();
+  });
   it.todo('should insert into a table');
   it.todo('should select from a table');
   it.todo('should close the connection');
